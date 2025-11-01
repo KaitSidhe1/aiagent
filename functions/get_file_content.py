91b02695b3d6c988c1f39975b5595fbde9a4d39b
KaitSidhe1 <Kait.Sidhe1@GMail.com>
2025-11-01 16:33:50 -0700

    add function declarations

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 MAX_CHARS = 10000
 
@@ -18,11 +19,21 @@ def get_file_content(working_directory, file_path):
             return file_content_string + f'[...File "{file_path}" truncated at 10000 characters]'
         return file_content_string
 
-
-
-
     except Exception as e:
         return (f"Error listing files: {e}")
       
     
-
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
